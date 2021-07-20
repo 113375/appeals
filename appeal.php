ccf09f228729e46525544d1d3df53dcd5510bc40
113375 <knyazevdima05@gmail.com>
2021-07-20 20:21:44 +0300

    подключил jquery

--- appeal.php
+++ appeal.php
@@ -160,6 +160,8 @@
     <script src="js/base.js"></script>
     <script src="js/appeal.js"></script>
     <script src="js/modalWin.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="typehead/bootstrap3-typeahead.js"></script>
 
 
 </body>
