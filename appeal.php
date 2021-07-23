64f589f9c2ce6e7c6bcd2af1caaa17fec7245d98
113375 <knyazevdima05@gmail.com>
2021-07-23 12:28:45 +0300

    Добавил id на input

--- appeal.php
+++ appeal.php
@@ -68,7 +68,8 @@
                         <img src="icons/checkmark.png" alt="">
                     </div>
                 </div>
-                <div class="appeal-margin-left appeal-button button purple list" id="instances-button" type-win="choice-dest">
+                <div class="appeal-margin-left appeal-button button purple list" id="instances-button"
+                    type-win="choice-dest">
                     Отметь адресатов
                 </div>
                 <div class="appeal-margin-left appeal-choice-text">
@@ -84,7 +85,7 @@
                     </div>
                 </div>
                 <div class="appeal-margin-left textarea-apppeal-block">
-                    <textarea placeholder="Опишите проблему" id="" rows="9"></textarea>
+                    <textarea placeholder="Опишите проблему" id="text-area" rows="9"></textarea>
                 </div>
             </div>
 
@@ -123,19 +124,22 @@
                 <div class="appeal-margin-left all-inputs">
                     <!-- Набор всех input внизу страницы-->
                     <div>
-                        <input type="text" name="" placeholder="Фамилия">
+                        <input type="text" name="" placeholder="Фамилия" id="input-surname"> <!-- input-surname-->
                     </div>
                     <div>
-                        <input type="text" name="" placeholder="Имя">
+                        <input type="text" name="" placeholder="Имя" id="input-name"> <!-- input-name-->
                     </div>
                     <div>
-                        <input type="text" name="" placeholder="Отчество(если есть)">
+                        <input type="text" name="" placeholder="Отчество(если есть)" id="input-patronymic">
+                        <!-- input-patronymic-->
                     </div>
                     <div>
-                        <input type="text" name="" placeholder="Должность(если есть)">
+                        <input type="text" name="" placeholder="Должность(если есть)" id="input-position">
+                        <!-- input-position-->
                     </div>
                     <div>
-                        <input type="email" name="" placeholder="Электронная почта">
+                        <input type="email" name="" placeholder="Электронная почта" id="input-email">
+                        <!-- input-email-->
                     </div>
 
                 </div>
@@ -144,7 +148,7 @@
 
         </div>
         <div class="button-block">
-            <div class="button-to-appeals button purple bottom-button letter big">
+            <div class="button-to-appeals button purple bottom-button letter big" id="send-buuton">
                 Отправить обращение
             </div>
         </div>
